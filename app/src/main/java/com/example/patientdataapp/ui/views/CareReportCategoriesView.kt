2debd4a516c905d7efc5c9b80795463bc7413e45
Noroxs <thomas.ramson@gmail.com>
2025-06-16 09:08:42 +0200

    Update CareReportCategoriesView

--- app/src/main/java/com/example/patientdataapp/ui/views/CareReportCategoriesView.kt
+++ app/src/main/java/com/example/patientdataapp/ui/views/CareReportCategoriesView.kt
@@ -1,16 +1,18 @@
 package com.example.patientdataapp.ui.views
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,25 +21,83 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.patientdataapp.data.data
-import com.example.patientdataapp.ui.viewmodel.PatientListCellViewModel
-
+import com.example.patientdataapp.datamodel.CategoryType
+import androidx.compose.material3.Icon
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun CareReportCategoryView() {
+fun CareReportCategoryView(category: CategoryType, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .padding(horizontal = 2.dp, vertical = 5.dp)
+            .shadow(
+                elevation = 2.dp,
+                shape = RoundedCornerShape(16.dp),
+                clip = false
+            )
+            .background(
+                color = MaterialTheme.colorScheme.background,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .border(
+                width = 1.dp,
+                shape = RoundedCornerShape(16.dp),
+                color = Color.DarkGray
+            )
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .background(category.color)
+                .padding(horizontal = 5.dp, vertical = 5.dp)
+                .height(10.dp)
+        ) {
+            category.image?.let {
+                Icon(
+                    category.image,
+                    contentDescription = category.title,
+                )
+                Spacer(modifier = Modifier.width(1.dp))
+            }
 
+            Text(
+                text = category.title,
+                style = MaterialTheme.typography.titleLarge,
+                fontSize = 7.sp
+            )
+        }
+    }
 }
 
-//CareReport(
-//id = "f7fecf7f-72e9-4123-80eb-a4ed39cd263f",
-//createdBy = "Fr. Saad",
-//createdAt = Instant.parse("2022-05-05T10:45:13.533Z"),
-//text = "Hat heute nicht gut mitgemacht, sie war schlecht gelaunt und wollte an den Beschäftigungangeboten nicht teilnehmen.",
-//categories = listOf("Teilnahme", "Aktivität")
-
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun CareReportCategoriesView(categories: List<CategoryType>) {
+    FlowRow(
+        horizontalArrangement = Arrangement.Start,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(2.dp)
+    ) {
+        for (category in categories) {
+            CareReportCategoryView(category = category)
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun CareReportCategoriesViewPreview() {
-    Text("Hallo")
+    //CareReportCategoryView(CategoryType("Wohlbefinden"))
+    CareReportCategoriesView(
+        listOf(
+            CategoryType("Wohlbefinden"),
+            CategoryType("Gesundheit"),
+            CategoryType("Teilnahme"),
+            CategoryType("Aktivität"),
+            CategoryType("Hygiene"),
+            CategoryType("Unknown")
+            )
+    )
 }
