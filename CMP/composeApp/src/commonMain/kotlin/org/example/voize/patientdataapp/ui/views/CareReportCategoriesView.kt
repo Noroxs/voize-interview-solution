c5b0bf4589c5bbcbf4e0b07250ec98ca921792a1
Noroxs <thomas.ramson@gmail.com>
2025-07-14 11:35:43 +0200

    Fix missing text bug

--- CMP/composeApp/src/commonMain/kotlin/org/example/voize/patientdataapp/ui/views/CareReportCategoriesView.kt
+++ CMP/composeApp/src/commonMain/kotlin/org/example/voize/patientdataapp/ui/views/CareReportCategoriesView.kt
@@ -1,10 +1,7 @@
 package com.example.patientdataapp.ui.views
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
@@ -12,23 +9,22 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.patientdataapp.datamodel.CategoryType
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Surface
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.patientdataapp.ui.icons.HygieneIcon
+import com.example.patientdataapp.datamodel.CategoryType
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun PillView(
@@ -56,14 +52,19 @@ fun PillView(
                 Icon(
                     icon,
                     contentDescription = text,
+                    modifier = Modifier.size(8.dp)
                 )
                 Spacer(modifier = Modifier.width(1.dp))
             }
 
             Text(
                 text = text,
+                fontSize = 7.sp,
+                lineHeight = 8.sp,
+                maxLines = 1,
                 style = MaterialTheme.typography.titleLarge,
-                fontSize = 7.sp
+                modifier = Modifier
+                    .padding(0.dp)
             )
         }
     }
@@ -91,7 +92,7 @@ fun CareReportCategoriesView(categories: List<CategoryType>) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview()
 @Composable
 fun CareReportCategoriesViewPreview() {
     CareReportCategoriesView(
